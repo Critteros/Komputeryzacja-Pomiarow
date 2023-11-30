c7f5b7b56d1f78cdb2fedc15cf86a1e1f082dbbd
Szymon Kozioł <contact@critteros.dev>
2023-11-30 17:54:34 +0100

    Qt Setup

--- termostat/gui.py
+++ termostat/gui.py
@@ -1,2 +1,28 @@
+import sys
+
+from qtmodern import styles
+
+from PyQt6.QtWidgets import QPushButton, QApplication, QMainWindow
+
+
+class MainWindow(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Termostat")
+        self.setAutoFillBackground(True)
+
+        button = QPushButton("Click me!")
+        self.setCentralWidget(button)
+
+        self.setMinimumSize(800, 600)
+
+
 def main():
-    print("gui")
+    app = QApplication(sys.argv)
+
+    styles.dark(app)
+
+    window = MainWindow()
+    window.show()
+
+    app.exec()
